d80b14a75037f081b2eb10c7d084b243aefdf532
nightcruise <1315235301@qq.com>
2024-08-23 13:59:01 +0800

    update pagination ability

--- community/src/main/java/life/community/dto/PaginationDTO.java
+++ community/src/main/java/life/community/dto/PaginationDTO.java
@@ -15,22 +15,42 @@ public class PaginationDTO {
     private Integer currentPage;
     private List<Integer> pages = new ArrayList<>();
     private Integer totalPageCount;
+    // 可见页码数量，可以修改为你想要的值
+    private final int visiblePages = 4;
 
     public void setPagination( Integer totalPageCount, Integer page) {
 
         this.currentPage = page;
         this.totalPageCount = totalPageCount;
 
-        pages.add(currentPage);
-        for (int i = 1; i <= 3; i++) {
-            if (currentPage - i > 0){
-                // 添加currentPage左边的页码
-                pages.add(0, currentPage - i);
+        int half = visiblePages / 2;
+
+        int startPage;
+        int endPage;
+
+        // 确定起始页码和结束页码
+        if (visiblePages % 2 == 0) {
+            // 偶数情况，当前页略偏左
+            startPage = Math.max(1, currentPage - half + 1);
+            endPage = Math.min(totalPageCount, currentPage + half);
+        } else {
+            // 奇数情况，当前页居中
+            startPage = Math.max(1, currentPage - half);
+            endPage = Math.min(totalPageCount, currentPage + half);
+        }
+
+        // 如果页码数量不足可见页码数量，则进行调整
+        if (endPage - startPage + 1 < visiblePages) {
+            if (startPage == 1) {
+                endPage = Math.min(totalPageCount, startPage + visiblePages - 1);
+            } else if (endPage == totalPageCount) {
+                startPage = Math.max(1, endPage - visiblePages + 1);
             }
-            if (currentPage + i <= totalPageCount ){
-                // 添加currentPage右边的页码
-                pages.add(currentPage + i);
-            }
+        }
+
+        // 添加页码到列表
+        for (int i = startPage; i <= endPage; i++) {
+            pages.add(i);
         }
 
         // 是否展示上一页按钮
